cc8f72fffc5b39d64216ca5123c5ac02a7c0b549
Bogdan-design <bogdanbw@gmail.com>
2024-03-26 23:05:45 +0100

    location

--- component/layout/header/headerPage.tsx
+++ component/layout/header/headerPage.tsx
@@ -5,31 +5,74 @@ import {links} from "@/lid/data";
 import Link from "next/link";
 import {Menu} from "@/component/menu/menu";
 import {useTranslation} from "next-i18next";
+import {Swiper, SwiperSlide} from "swiper/react";
+import {monumentExtended} from "@/fonts/fonts";
+import {useMobileContext} from "@/app/context/mobile.context";
 import s from './header.module.scss'
 
 export const HeaderPage = () => {
     const {t} = useTranslation()
-
+    const {isMobile}= useMobileContext()
 
     return (
         <header className={s.header}>
             <div className={s.container}>
-
                 <Link href={'/'}>
                     <Logo/>
                 </Link>
-                <div className={s.options}>
-                    <nav>
-                        <div className={s.links}>
-                            {links.map((l) => (
-                                <Link className={s.link} key={l.hash} href={`/${l.hash}`}>{t(`${l.name}`)}</Link>
-                            ))}
+                {isMobile ? <Swiper
+                        spaceBetween={20}
+                        slidesPerView={2.2}
+
+                    >
+
+                        <div className={s.options}>
+                            <nav>
+                                <div className={s.links}>
+                                    {links.map((l) => (
+                                        <SwiperSlide key={l.hash}>
+                                            <Link style={monumentExtended.style} className={s.link}
+                                                  href={`/${l.hash}`}>{t(l.name)}</Link>
+                                        </SwiperSlide>
+                                    ))}
+                                </div>
+                            </nav>
+                            <SwiperSlide style={{top: "6px"}}>
+                                <div style={{display: "flex"}}>
+                                    <Menu/>
+                                </div>
+                            </SwiperSlide>
                         </div>
-                    </nav>
-                    <Menu/>
-                </div>
+                    </Swiper> :
+                    <div className={s.options}>
+                        <nav>
+                            <div className={s.links}>
+                                {links.map((l) => (
+                                    <Link style={monumentExtended.style} className={s.link} key={l.hash}
+                                          href={`/${l.hash}`}>{t(l.name)}</Link>
+                                ))}
+                            </div>
+                        </nav>
+                        <Menu/>
+                    </div>
+                }
             </div>
+                {/*<div className={s.container}>*/}
+                {/*    <Link href={'/'}>*/}
+                {/*        <Logo/>*/}
+                {/*    </Link>*/}
+                {/*    <div className={s.options}>*/}
+                {/*        <nav>*/}
+                {/*            <div className={s.links}>*/}
+                {/*                {links.map((l) => (*/}
+                {/*                    <Link className={s.link} key={l.hash} href={`/${l.hash}`}>{t(`${l.name}`)}</Link>*/}
+                {/*                ))}*/}
+                {/*            </div>*/}
+                {/*        </nav>*/}
+                {/*        <Menu/>*/}
+                {/*    </div>*/}
+                {/*</div>*/}
         </header>
-    );
+);
 };
 
